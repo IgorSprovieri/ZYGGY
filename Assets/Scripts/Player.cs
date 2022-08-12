6b37cf46f49b7edaf6f7af73ffd6cfea524b4092
igsp1 <igsp1@hotmail.com>
2022-08-11 21:33:40 -0300

    Climb and double jump

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,9 +8,11 @@ public class Player : MonoBehaviour
     Vector3 CheckPoint;
     Vector2 MovementInput;
     float ScaleX = 1.0f;
-    bool CollisionWall, CollisionFloor, JumpInput;
+    public bool CollisionWall, CollisionFloor, JumpInput, ClimbMode, DoubleJump;
     public Rigidbody2D rb2D;
-    
+    public LayerMask Ground;
+    public bool b = false;
+
     void OnMove(InputValue value)
     {
         MovementInput = value.Get<Vector2>();
@@ -28,7 +30,6 @@ public class Player : MonoBehaviour
 
     void OnJump(InputValue value)
     {
-
         if (value.isPressed)
         {
             JumpInput = true;
@@ -45,23 +46,68 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(CollisionWall == false)
+        float translateVelocityX = MovementInput.x * Time.deltaTime;
+        float translateVelocityY = 0.0f;
+
+        bool isTouchingUp = Physics2D.Raycast(new Vector2(transform.position.x, transform.position.y + 0.3f), transform.right, 0.4f, Ground);
+        bool isTouchingDown = Physics2D.Raycast(new Vector2(transform.position.x, transform.position.y), transform.right, 0.4f, Ground); 
+
+        if (isTouchingUp == true && isTouchingDown == true)
         {
-            Vector3 translateVelocity = new Vector3(MovementInput.x * Time.deltaTime, 0.0f, 0.0f);
-            transform.Translate(translateVelocity * 2.0f);
+            translateVelocityX = 0.0f;
+            translateVelocityY = MovementInput.y * Time.deltaTime;
+            rb2D.simulated = false;
+            rb2D.velocity = new Vector2(0, 0);
+            //rb2D.isKinematic = true;
+            b = false;
         }
+
+        if (isTouchingUp == false && isTouchingDown == true)
+        {
+            translateVelocityX = 0.0f;
+            translateVelocityY = 0.0f;
+            rb2D.simulated = true;
+            //rb2D.isKinematic = false;
+
+            if (b == false)
+            {
+                rb2D.AddForce(transform.up * 270.0f);
+                b = true;
+            }
+        }
+
+        if (isTouchingUp == true && isTouchingDown == false)
+        {
+            translateVelocityX = MovementInput.x * Time.deltaTime;
+            translateVelocityY = 0.0f;
+            rb2D.simulated = true;
+            //rb2D.isKinematic = false;
+        }
+
+        transform.Translate(translateVelocityX * 2.0f, translateVelocityY * 2.0f, 0.0f);
 
         Vector3 FinalScale = new Vector3(ScaleX * 0.75f, 0.75f, 0);
         transform.localScale = Vector3.Lerp(transform.localScale, FinalScale, 5.0f * Time.deltaTime);
 
-        if(CollisionWall == true || CollisionFloor == true)
+        if(CollisionFloor == true)
         {
-            if(JumpInput == true)
+            DoubleJump = false;
+            if (JumpInput == true)
             {
                 rb2D.AddForce(transform.up * 270.0f);
                 JumpInput = false;
             }
-
+        }
+        else
+        {
+            if (JumpInput == true)
+            {
+                if (DoubleJump == false)
+                {
+                    rb2D.AddForce(transform.up * 150.0f);
+                    DoubleJump = true;
+                }
+            }
         }
 
         if(transform.position.y < -7)
@@ -75,6 +121,9 @@ public class Player : MonoBehaviour
         transform.position = CheckPoint;
         transform.eulerAngles = new Vector3(0, 0, 0);
         transform.localScale = new Vector3(0.75f, 0.75f, 0);
+        CollisionWall = false;
+        CollisionFloor = false;
+        JumpInput = false;
     }
 
     void SaveCheckPoint()
@@ -84,7 +133,7 @@ public class Player : MonoBehaviour
 
     void OnCollisionEnter2D(Collision2D Col)
     {
-        if(Col.transform.gameObject.tag == "Wall")
+        if (Col.transform.gameObject.tag == "Wall")
         {
             CollisionWall = true;
         }
@@ -99,10 +148,6 @@ public class Player : MonoBehaviour
     }
     void OnCollisionExit2D(Collision2D Col)
     {
-        if (Col.transform.gameObject.tag == "Wall")
-        {
-            CollisionWall = false;
-        }
         if (Col.transform.gameObject.tag == "Floor")
         {
             CollisionFloor = false;
@@ -114,6 +159,11 @@ public class Player : MonoBehaviour
         if (Col.transform.gameObject.tag == "Respawn")
         {
             SaveCheckPoint();
+        }
+
+        if (Col.transform.gameObject.tag == "Finish")
+        {
+            //nextlevel
         }
 
         Col.transform.gameObject.SetActive(false);
